fde868cdf0ca9c5572160f9762afe06955757358
Oliwia Karbowa <242416@edu.p.lodz.pl>
2024-03-19 18:42:19 +0100

    Etap 0

---
+++ Etap0/Calculator/Calculator.cs
@@ -0,0 +1,30 @@
+﻿namespace TPW
+{
+    public class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public double add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Concurrent programming
+
+## Working Group
+
+| Name Surname (initials) | GUID                                     |
+| ----------------------- | ---------------------------------------- |
+| MJ                      | `{802D6D4B-99F3-4C18-9A5C-C1A909C7904A}` |
+| OK                      | `{90AF39C8-B6CC-45C7-8190-E4288A9C031D}` |
